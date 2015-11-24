28bbd5ea0e9c1ba83f11921366d9ce7bce4757da
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-11-24 09:45:31 -0800

    Updating repostipdedia folder organization

---
+++ js/variables.js
@@ -0,0 +1,23 @@
+var user;
+
+//var keywords = [null, null, null, null, null, null, null, null, null, null];
+
+function Entry(entrytitle,entryurl,author,incomplete,biased,complex,nocitations) {
+  this.entrytitle = entrytitle;
+  this.entryurl = entryurl;
+  this.author = author;
+  this.incomplete = incomplete;
+  this.biased = biased;
+  this.complex = complex;
+  this.nocitations = nocitations;
+}
+
+function Post(postheader,referencedentry,referencedentry,keywords,currentusersvote,upvotes,downvotes) {
+  this.postheader = postheader;
+  this.referencedentry = referencedentry;
+  this.keywords = keywords;
+  this.currentusersvote = currentusersvote;
+  this.upvotes = upvotes;
+  this.downvotes = downvotes;
+}
+
