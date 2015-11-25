0069c5a44ae093d6ef595ff2cede1d16d6ad1b36
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-11-25 15:37:56 -0800

    Added ability to hide and show hamburger menu using JavaScript

--- js/variables.js
+++ js/variables.js
@@ -1,9 +1,29 @@
-var titleheight = document.getElementById("titleheight").clientHeight;
+var titleheight = document.getElementById('titleheight').clientHeight;
 
 function setHeight(newheight) {
-  document.getElementById("voteboxheight").style.height = newheight;
+  document.getElementById('voteboxheight').style.height = newheight;
 }
 window.onload = setHeight(titleheight+'px');
+
+
+//errors in this area
+var hamburgerbutton = document.getElementById('hamburgerbutton'); 
+var hamburgerlinks = document.getElementById('hamburgerlinks');
+
+hamburgerlinks.alterVisibility = function changevisibility() {
+  if(this.style.visibility == "visible") {
+    this.style.visibility="hidden";
+  }
+  else {
+    this.style.visibility="visible";
+  }
+  //INSERT PAGE REFRESH???
+}
+
+hamburgerbutton.addEventListener('click',function() {hamburgerlinks.alterVisibility();},false);
+
+
+
 
 //var keywords = [null, null, null, null, null, null, null, null, null, null];
 
