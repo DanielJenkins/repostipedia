c948559ec0dcded93240f8280ecdd25f59c44f92
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2015-12-01 14:31:14 -0800

    Checking in current progress. Currently working on adding functionality to vote buttons

--- js/variables.js
+++ js/variables.js
@@ -1,12 +1,3 @@
-var titleheight = document.getElementById('titleheight').clientHeight;
-
-function setHeight(newheight) {
-  document.getElementById('voteboxheight').style.height = newheight;
-}
-window.onload = setHeight(titleheight+'px');
-//window.onload = setHeight('100px');
-
-
 //Hamburger Button
 var hamburgerbutton = document.getElementById('hamburgerbutton'); 
 var hamburgerlinks = document.getElementById('hamburgerlinks');
@@ -17,9 +8,9 @@ hamburgerlinks.alterVisibility = function changevisibility() {
   else {
     this.style.visibility="visible";
   }
-  //INSERT PAGE REFRESH???
 }
 hamburgerbutton.addEventListener('click',function() {hamburgerlinks.alterVisibility();},false);
+
 
 //Hamburger Navigation
 var homelink = document.getElementById('homelink');
@@ -32,7 +23,7 @@ var viewpost = document.getElementById('viewpost');
 var listofposts = document.getElementById('listofposts');
 var usingthissite = document.getElementById('usingthissite');
 
-var currentpage = viewpost;
+var currentpage = listofposts;
 var newpage = 'empty';
 function loadsegment(theNewpage) {
   newpage = theNewpage;
@@ -51,9 +42,76 @@ newpostlink.addEventListener('click',function() {loadsegment(newpost)},false);
 usingthissitelink.addEventListener('click',function() {loadsegment(usingthissite)},false);
 
 
+//View Post Title Adjustments
+var titleheight = document.getElementById('titleheight').clientHeight;
+
+function setHeight(newheight) {
+  document.getElementById('voteboxheight').style.height = newheight;
+}
+window.onload = setHeight(titleheight+'px');
 
 
-//var keywords = [null, null, null, null, null, null, null, null, null, null];
+//Entry
+function Entry(position, title, content, upvotecount, downvotecount, datecreated){
+  this.position=position;
+  this.title=title;
+  this.content=content;
+  this.upvotecount=upvotecount;
+  this.downvotecount=downvotecount;
+  this.datecreated=datecreated;
+  this.post1totalpopularity=upvotecount-downvotecount;
+  this.upvote = function() {
+    this.upvotecount += 1;
+  }
+  this.downvote = function() {
+    this.downvotecount += 1;
+  }
+}
+
+//Normally, this data would be user generated and stored in a separate file.
+var titleEntryA = 'We should go to the moon again';
+var contentEntryA = 'We should totally go to the moon again! Check out how cool the Apollo missions were in this great piece. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore';
+var upvotecountEntryA = 999;
+var downvotecountEntryA = 12;
+var datecreatedEntryA = new Date();
+
+
+
+
+
+
+
+//note only
+ var d = new Date();
+ d.setDate(d.getDate()-5);
+
+
+
+
+
+//Post1
+var post1title = document.getElementById('post1title'); 
+var post1content = document.getElementById('post1content'); 
+var post1upvotecount = document.getElementById('post1upvotecount'); 
+var post1downvotecount = document.getElementById('post1downvotecount'); 
+
+post1title.appendChild(post1.title);
+post1content.appendChild(post1.content);
+post1upvotecount.appendChild(post1.upvotecount);
+post1downvotecount.appendChild(post1.downvotecount);
+
+var post1upvote = document.getElementById('post1upvote'); 
+var post1downvote = document.getElementById('post1downvote');
+post1upvote.addEventListener('click',function() {post1.upvote();},false);
+post1downvote.addEventListener('click',function() {hamburgerlinks.alterVisibility();},false);
+
+
+
+
+
+
+
+
 
 /*
 function Entry(entrytitle,entryurl,author,incomplete,biased,complex,nocitations) {
@@ -66,6 +124,7 @@ function Entry(entrytitle,entryurl,author,incomplete,biased,complex,nocitations)
   this.nocitations = nocitations;
 }
 
+var keywords = [null, null, null, null, null, null, null, null, null, null];
 function Post(postheader,referencedentry,referencedentry,keywords,currentusersvote,upvotes,downvotes) {
   this.postheader = postheader;
   this.referencedentry = referencedentry;
